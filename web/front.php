c11cfa322e2323ea4033de09c6a5d3b57191253d
Enrique Garcia <enrique@inodata.com.mx>
2013-03-11 03:24:47 -0600

    Implementa el controlador utilizando HttpKernel.

--- web/front.php
+++ web/front.php
@@ -5,6 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
 
 function render_template($request){
 	$pages_dir = __DIR__.'/../src/pages/';
@@ -21,11 +22,13 @@ $routes = include __DIR__.'/../src/app.php';
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+$resolver = new HttpKernel\Controller\ControllerResolver();
 
 try{
 	$request->attributes->add($matcher->match($request->getPathInfo()));
-	//OJO: aqui el parametro callback se extrae de la ruta para que sea valido
-	$response = call_user_func($request->attributes->get('_controller'), $request);
+	$controller = $resolver->getController($request);
+	$arguments = $resolver->getArguments($request, $controller);
+	$response = call_user_func_array($controller, $arguments);
 }catch (Routing\Exception\ResourceNotFoundException $e){
 	$response = new Response('Not Found', 404);
 }catch (Exception $e){
